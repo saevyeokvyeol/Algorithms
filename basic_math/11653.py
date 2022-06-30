30612b083123b12d7d6b607f499ff776bb9e29d2
yuda <yudajgim@gmail.com>
2022-06-30 21:12:04 +0900

    basic_math 4948 11653

--- basic_math/11653.py
+++ basic_math/11653.py
@@ -1,7 +1,6 @@
 import sys
 
-N = int(sys.stdin.readline())
-
+N = 1000
 while N > 1:
   for i in range(2, N + 1):
     if N % i == 0:
--- basic_math/4948.py
+++ basic_math/4948.py
@@ -1,11 +1,17 @@
-n = 1000
-a = [True]*(n + 1)
+while True:
+  n = int(input())
 
-for i in range(2, int((n * 2) ** 0.5) + 1):
-  if a[i] == True:
-    for j in range(i + i, n + 1, i):
-      a[j] = False
+  if(n == 0): break
 
-for i in range(2, n + 1):
-  if a[i] == True:
-    print(i)
+  sosu = [True]*((n * 2 + 1))
+  result = 0
+
+  for i in range(2, int((n * 2) ** 0.5) + 1):
+    if sosu[i] == True:
+      for j in range(i + i, ((n * 2) + 1), i):
+        sosu[j] = False
+
+  for i in range(n + 1, ((n * 2) + 1)):
+    if sosu[i] == True:
+      result += 1
+  print(result)
