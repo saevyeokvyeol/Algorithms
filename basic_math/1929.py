f8290cc5877c5f7c56910576a3f558b9538f9c63
yuda <yudajgim@gmail.com>
2022-07-01 22:15:44 +0900

    basic_math 1929 풀이 추가

--- basic_math/1929.py
+++ basic_math/1929.py
@@ -1,5 +1,6 @@
 import sys
 
+# 1차 풀이
 num = list(map(int, sys.stdin.readline().split()))
 
 for i in range(num[0], num[1] + 1):
@@ -11,4 +12,20 @@ for i in range(num[0], num[1] + 1):
       break
 
   if check == 0 and i != 1:
+    print(i)
+
+# 에라토스테네스의 체로 응용
+num = list(map(int, sys.stdin.readline().split()))
+
+sosu = [True] * (num[1] + 1)
+
+sosu[1] = False
+
+for i in range(2, int(num[1] ** 0.5) + 1):
+  if sosu[i] == True:
+    for j in range(i * 2, num[1] + 1, i):
+      sosu[j] = False
+
+for i in range(num[0], num[1] + 1):
+  if sosu[i] == True:
     print(i)
