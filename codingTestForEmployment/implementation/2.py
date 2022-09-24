4faa35f24fc772d2478719e1a60857886c445d72
yuda <yudajgim@gmail.com>
2022-09-24 21:36:52 +0900

    구현(이것이 취업을 위한 코딩 테스트다) 4문제

---
+++ codingTestForEmployment/implementation/2.py
@@ -0,0 +1,21 @@
+n = int(input())
+result = 0
+for i in range(n + 1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                result += 1
+print(result)
+
+# 예시
+# H를 입력받기
+h = int(input())
+
+count = 0
+for i in range(h + 1):
+    for j in range(60):
+        for k in range(60):
+            # 매 시각 안에 '3'이 포함되어 있다면 카운트 증가
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+print(count)
---
+++ codingTestForEmployment/implementation/3.py
@@ -0,0 +1,32 @@
+# I = input()
+# X = [2, -2]
+# Y = [1, -1]
+# cnt = 0
+# for x in X:
+#     for y in Y:
+#         if ord("a") <= ord(I[0]) + x <= ord("h") and 1 <= int(I[1]) + y <= 8:
+#             cnt += 1
+#         if ord("a") <= ord(I[0]) + y <= ord("h") and 1 <= int(I[1]) + x <= 8:
+#             cnt += 1
+# print(cnt)
+
+# 예시 코드
+# 현재 나이트의 위치 입력받기
+input_data = input()
+row = int(input_data[1])
+column = int(ord(input_data[0])) - int(ord('a')) + 1
+
+# 나이트가 이동할 수 있는 8가지 방향
+steps = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
+
+# 8가지 방향에 대하여 각 위치로 이동이 가능한지 확인
+result = 0
+for step in steps:
+    # 이동하고자 하는 위치 확인
+    next_row = row + step[0]
+    next_column = column + step[1]
+    # 해당 위치로 이동이 가능하다면 카운트 증가
+    if next_row >= 1 and next_row <= 8 and next_column >= 1 and next_column <= 8:
+        result += 1
+
+print(result)
