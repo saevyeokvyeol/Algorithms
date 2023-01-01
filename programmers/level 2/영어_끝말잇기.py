4e8d48fb638f95976e3b975e34d2122c02a56dbc
yuda <yudajgim@gmail.com>
2023-01-01 20:38:23 +0900

    프로그래머스 Lv. 2 영어 끝말잇기(+)

--- programmers/level 2/영어_끝말잇기.py
+++ programmers/level 2/영어_끝말잇기.py
@@ -1,7 +1,14 @@
+# 첫 번째 풀이
 def solution(n, words):
-    word = ""
     for i in range(len(words)):
-        if i != words.index(words[i])or len(word) > 0 and words[i][0] != word[-1]:
+        if i != words.index(words[i]) or len(word) > 0 and words[i][0] != word[-1]:
             return [i % n + 1, int(i // n) + 1]
         word = words[i]
+    return [0, 0]
+
+# 두 번째 풀이
+def solution(n, words):
+    for i in range(1, len(words)):
+        if words[i] in words[:i] or words[i][0] != words[i - 1][-1]:
+            return [i % n + 1, int(i // n) + 1]
     return [0, 0]
