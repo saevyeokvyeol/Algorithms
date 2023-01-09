39dea19ae97748119c4d3c6738b884f9d32d66a7
yuda <yudajgim@gmail.com>
2023-01-09 20:44:46 +0900

    implementation 1672

---
+++ BOJ/implementation/bronze2/1672.py
@@ -0,0 +1,10 @@
+dna = [['A', 'C', 'A', 'G'], ['C', 'G', 'T', 'A'], ['A', 'T', 'C', 'G'], ['G', 'A', 'G', 'T']]
+a = {'A':0, 'G':1, 'C':2, 'T':3}
+
+n = int(input())
+s = input()
+end = s[-1]
+for i in s[::-1][1:]:
+    print(i)
+    end = dna[a[i]][a[end]]
+print(end)
