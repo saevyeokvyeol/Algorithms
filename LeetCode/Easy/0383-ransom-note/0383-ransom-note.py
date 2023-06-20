fc722cc845d0c0a8ab25b8d3c9e576dd5e9d8aac
YUDA <88480600+saevyeokvyeol@users.noreply.github.com>
2023-06-20 18:31:45 +0900

    Time: 69 ms (67.89%), Space: 16.6 MB (25.07%) - LeetHub

--- LeetCode/Easy/0383-ransom-note/0383-ransom-note.py
+++ LeetCode/Easy/0383-ransom-note/0383-ransom-note.py
@@ -1,8 +1,11 @@
+from collections import Counter
+
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        result = True
-        for i in range(ord("a"), ord("z") + 1):
-            if ransomNote.count(chr(i)) > magazine.count(chr(i)):
-                result = False
-                break
-        return result
+        note_dic = Counter(ransomNote)
+        mag_dic = Counter(magazine)
+
+        for n in note_dic:
+            if note_dic[n] > mag_dic[n]:
+                return False
+        return True
