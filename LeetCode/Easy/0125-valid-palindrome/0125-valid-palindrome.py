2c970fa9b8d22a90224d31afabc34a1fd2bdc423
YUDA <88480600+saevyeokvyeol@users.noreply.github.com>
2023-06-25 09:09:10 +0900

    Time: 62 ms (64.31%), Space: 17.9 MB (22.48%) - LeetHub

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.py
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,19 +1,5 @@
 class Solution:
-    # 내 풀이
     def isPalindrome(self, s: str) -> bool:
-        s = "".join([i for i in s.lower() if 'a' <= i <= 'z' or '0' <= i <= '9'])
+        s = "".join([char for char in s.lower() if char.isalnum()])
         print(s, s[::-1])
         return True if s == s[::-1] else False
-
-    # 리스트로 변환해 풀기
-    def isPalindrome(self, s: str) -> bool:
-        strs = []
-        for char in s:
-            if char.isalnum(): # isalnum(): 영문자, 숫자 판별 함수
-                strs.append(char.lower())
-
-        while len(strs) > 1:
-            if strs.pop(0) != strs.pop():
-                return False
-        
-        return True
